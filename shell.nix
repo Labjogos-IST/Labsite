2e77d526b7f19a257a10590de349cf6c1e55aadd
Petingoso <petingavasco@protonmail.com>
2026-05-19 01:17:04 +0100

    pretty okay rev

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
+let
+  # Fetch the cutting-edge unstable channel tracking branch
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { };
+in
 pkgs.mkShellNoCC {
   packages = [
-    pkgs.hugo
+    unstable.hugo # Uses latest unstable Hugo release
     pkgs.pnpm
     pkgs.nodejs
   ];
